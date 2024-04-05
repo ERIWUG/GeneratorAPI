249cab4f63cd32e3ca2315e5caaeebee4f8e91d3
ERIWUG <e.belyi2013@gmail.com>
2024-04-05 16:02:02 +0300

    refactor: change properties in QuestionDataWithProbability class

--- GeneratorAPI/Models/QuestionDataWithProbability.cs
+++ GeneratorAPI/Models/QuestionDataWithProbability.cs
@@ -1,12 +1,22 @@
 ﻿namespace GeneratorAPI.Models
 {
-    public class QuestionDataWithProbability:QuestionData
+    public class QuestionDataWithProbability
     {
         public double probability { get; set; }
+        public string text { get; set; }
+        public int type { get; set; }
+        public bool flag { get; set; }
+        public int id { get; set; }
+        public bool hasImage { get; set; }
 
-        public QuestionDataWithProbability(string text, int type, bool flag, double probability) : base(text, type, flag)
+
+        public QuestionDataWithProbability(string text, int type, bool flag,  double probability,bool hasImage = false)
         {
             this.probability = probability;
+            this.text = text;
+            this.type = type;
+            this.flag = flag;
+            this.hasImage = hasImage;
         }
     }
 }
