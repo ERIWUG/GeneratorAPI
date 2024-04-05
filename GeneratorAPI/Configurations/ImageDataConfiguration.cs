4be3f481c1a6d4f2df631ae436784d7858e69b6b
ERIWUG <e.belyi2013@gmail.com>
2024-04-05 15:56:17 +0300

    Reapply "Add:  Configuration files for QuestionData, QuestionDataWithProbability, ImageData"
    This reverts commit 891f25f5a3c395501fb81e398de1d60b119b1cae.

---
+++ GeneratorAPI/Configurations/ImageDataConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using GeneratorAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GeneratorAPI.Configurations
+{
+    public class ImageDataConfiguration : IEntityTypeConfiguration<ImageData>
+    {
+        public void Configure(EntityTypeBuilder<ImageData> builder)
+        {
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        }
+    }
+}
---
+++ GeneratorAPI/Configurations/QuestionDataWithProbabilityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using GeneratorAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GeneratorAPI.Configurations
+{
+    public class QuestionDataWithProbabilityConfiguration : IEntityTypeConfiguration<QuestionDataWithProbability>
+    {
+        public void Configure(EntityTypeBuilder<QuestionDataWithProbability> builder)
+        {
+            builder.Property(x => x.id).ValueGeneratedOnAdd();
+        }
+    }
+}
