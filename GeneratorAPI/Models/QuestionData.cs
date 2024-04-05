5d0492f2af901eb75b57aa079ba8b2953278e1f7
ERIWUG <e.belyi2013@gmail.com>
2024-04-05 16:03:07 +0300

    refactor: add HasImageProperty

--- GeneratorAPI/Models/QuestionData.cs
+++ GeneratorAPI/Models/QuestionData.cs
@@ -10,11 +10,13 @@ namespace GeneratorAPI.Models
         public int type { get; set; }
         public bool flag { get; set; }
         public int id { get; set; }
-        public QuestionData(string text, int type, bool flag)
+        public bool hasImage { get; set; }
+        public QuestionData(string text, int type, bool flag, bool hasImage=false)
         {
             this.text = text;
             this.type = type;
             this.flag = flag;
+            this.hasImage = hasImage;
         }
         public QuestionData() { }
     }
