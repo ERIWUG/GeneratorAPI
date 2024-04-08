5845aebb8fa17899bfceab1b53d89ed60762bf02
ERIWUG <e.belyi2013@gmail.com>
2024-04-08 11:27:35 +0300

    feat: rewrite project structure and rewrite class structure

---
+++ GeneratorAPI/Models/Entities/QuestionDataEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeneratorAPI.Models.Entities
+{
+    [Serializable]
+    public class QuestionDataEntity
+    {
+        public string text { get; set; } = string.Empty;
+        public int type { get; set; } = 0;
+        public bool flag { get; set; } = false;
+        public Guid id { get; set; }
+        public decimal probability { get; set; } = decimal.Zero;
+        public bool hasImage { get; set; } = false;
+
+        public List<ImageDataEntity> images { get; set; } = [];
+
+    }
+}
