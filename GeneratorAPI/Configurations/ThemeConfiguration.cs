840a7364d9b2cc1a3b137060fdadb8bfa94b9a50
ERIWUG <e.belyi2013@gmail.com>
2024-04-10 18:00:48 +0300

    Feat: Add config file to ThemeEntity

---
+++ GeneratorAPI/Configurations/ThemeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using GeneratorAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GeneratorAPI.Configurations
+{
+    public class ThemeConfiguration : IEntityTypeConfiguration<ThemeEntity>
+    {
+        public void Configure(EntityTypeBuilder<ThemeEntity> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).ValueGeneratedOnAdd();
+        }
+    }
+}
