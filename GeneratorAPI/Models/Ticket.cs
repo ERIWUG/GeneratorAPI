36626d4fcea70fb0b5bdd86a88f319d4568d4f97
ERIWUG <e.belyi2013@gmail.com>
2024-04-10 16:08:16 +0300

    fix: rename question to Ticket

---
+++ GeneratorAPI/Models/Ticket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorAPI.Models
+{
+    [Serializable]
+    public class Ticket
+    {
+        public int QuestionID;
+        public List<int> AnswersID;
+        public int CorrectAnswer;
+        public string QuestionHash;
+
+        public Ticket(int questionID, List<int> answers, int correctAnswer, string questionHash)
+        {
+            this.QuestionID = questionID;
+            this.AnswersID = answers;
+            this.CorrectAnswer = correctAnswer;
+            QuestionHash = questionHash;
+        }
+
+        
+    };
+}
