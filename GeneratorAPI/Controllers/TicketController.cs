c50102c75eec89d2d8a60fd2299de400a8206c9c
ERIWUG <e.belyi2013@gmail.com>
2024-04-10 16:08:00 +0300

    Feat: Add ticket generator controller

---
+++ GeneratorAPI/Controllers/TicketController.cs
@@ -0,0 +1,25 @@
+﻿using GeneratorAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeneratorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private AppDbContext db = new AppDbContext();
+
+        [HttpGet("/api/Ticket/GetLinear/{amount}")]
+
+        public async Task<IActionResult> GetLinear(int id)
+        {
+            
+            
+            return Ok(Generator.GenerateLinear(db.TempQuestAns.Where(c=>c.QuestionID==id).ToArray(),5));
+            
+        }
+
+
+    }
+}
