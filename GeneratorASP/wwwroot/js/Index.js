95a95d2316f21c7e396315e527853b4c40390196
ERIWUG <e.belyi2013@gmail.com>
2024-04-11 15:55:08 +0300

    Feat: add CheckBox Logic

--- GeneratorASP/wwwroot/js/Index.js
+++ GeneratorASP/wwwroot/js/Index.js
@@ -6,7 +6,17 @@
     document.getElementById("div-checkbox-" + x).style.display = "flex";
 }
 
-function MyFunc1(){
+function MyFunc1(a,b){
      
-        document.getElementById("question-select-value").value = document.getElementById("question-select").value;
+    document.getElementById("question-select-value").value = document.getElementById("question-select").value;
+
+
+
+    for (let i = 1; i <= b; i++) {
+        document.getElementById("div-checkbox-value-"+i).checked==contains(a,document.getElementById("div-checkbox-value-"+i).value)
+    }
+}
+
+function contains(arr, elem) {
+    return arr.indexOf(elem) != -1;
 }
