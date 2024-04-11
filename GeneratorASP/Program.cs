f6e83975f63de741d13f8614b2a1e31da471df56
VictorMinsk <86519204+VictorMinsk@users.noreply.github.com>
2024-04-11 15:59:29 +0300

    feat: added QueToAnsRepository

--- GeneratorASP/Program.cs
+++ GeneratorASP/Program.cs
@@ -7,8 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+builder.Services.AddTransient<QuestionRepository>();
+builder.Services.AddTransient<AnswerRepository>();
+builder.Services.AddTransient<ThemeRepository>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
