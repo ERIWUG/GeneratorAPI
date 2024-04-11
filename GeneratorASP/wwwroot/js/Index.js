d25dd536e8df25f50c69d7b79d248f89bd36fe61
ERIWUG <e.belyi2013@gmail.com>
2024-04-11 12:47:33 +0300

    Fear: return css and js for Index

---
+++ GeneratorASP/wwwroot/js/Index.js
@@ -0,0 +1,12 @@
+﻿function MyFunc(a) {
+    for (let i = 1; i <= a; i++) {
+        document.getElementById("div-checkbox-" + i).style.display = "none";
+    }
+    var x = document.getElementById("theme-select").value;
+    document.getElementById("div-checkbox-" + x).style.display = "flex";
+}
+
+function MyFunc1(){
+     
+        document.getElementById("question-select-value").value = document.getElementById("question-select").value;
+}
