37168ae2eefd0deee8bd4c45990b40a98c35bd1f
ERIWUG <e.belyi2013@gmail.com>
2024-04-12 11:13:51 +0300

    fead: finally made editing page

--- GeneratorASP/wwwroot/js/Index.js
+++ GeneratorASP/wwwroot/js/Index.js
@@ -6,17 +6,24 @@
     document.getElementById("div-checkbox-" + x).style.display = "flex";
 }
 
-function MyFunc1(a,b){
-    console.log(a);
+function Func(b) {
     document.getElementById("question-select-value").value = document.getElementById("question-select").value;
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var c = this.responseText.slice(1, this.responseText.length - 1).split(',').map(Number);
+            alert(c);
+            for(let i = 1; i <= b; i++) {
+                document.getElementById("div-checkbox-value-" + i).checked = contains(c, Number(document.getElementById("div-checkbox-value-" + i).value));
+            }
+        
+        }
+    };
+    xhr.open('GET', '/api/QuesToAnswer/getAnswerIdFromQuestionId?Id=' + document.getElementById("question-select-value").value,true)
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
 
-
-
-    for (let i = 1; i <= b; i++) {
-        document.getElementById("div-checkbox-value-"+i).checked==contains(a,document.getElementById("div-checkbox-value-"+i).value)
-    }
 }
-
 function contains(arr, elem) {
     return arr.indexOf(elem) != -1;
 }
