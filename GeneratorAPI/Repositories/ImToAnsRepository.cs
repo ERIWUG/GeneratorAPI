9b27c73ae84194d09b9c2720e87bd20fc922fb4f
VictorMinsk <86519204+VictorMinsk@users.noreply.github.com>
2024-04-12 12:20:54 +0300

    feat: added some new repositories

---
+++ GeneratorAPI/Repositories/ImToAnsRepository.cs
@@ -0,0 +1,41 @@
+﻿using GeneratorAPI.Models;
+using GeneratorAPI.Models.Entities;
+using GeneratorAPI.Models.TempTable;
+
+namespace GeneratorAPI.Repositories
+{
+    public class ImToAnsRepository
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly AnswerRepository _answerRepository;
+
+        public ImToAnsRepository(AppDbContext context)
+        {
+            _dbContext = context;
+            _answerRepository = new AnswerRepository(context);
+        }
+
+        public async Task AddAnswersForImage(int imageId, List<int> answersIds)
+        {
+            foreach (var answerId in answersIds)
+            {
+                AnswerEntity answer = await _answerRepository.GetById(answerId);
+                ImToAns imToAns = new ImToAns
+                {
+                    ImageID = imageId,
+                    AnswerID = answerId,
+                    ThemeAnswer = answer.ThemeEntity.Id
+                };
+                await _dbContext.AddAsync(imToAns);
+            }
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int[]> GetAnswerFromImageId(int id)
+        {
+            var c = _dbContext.ImagesToAnswers.Where(c => c.ImageID == id).Select(c => c.AnswerID).ToArray();
+            return c;
+        }
+    }
+}
---
+++ GeneratorAPI/Repositories/QuesToImRepository.cs
@@ -0,0 +1,43 @@
+﻿using GeneratorAPI.Models;
+using GeneratorAPI.Models.Entities;
+using GeneratorAPI.Models.TempTable;
+
+namespace GeneratorAPI.Repositories
+{
+    public class QuesToImRepository
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly ImageRepository _imageRepository;
+
+        public QuesToImRepository(AppDbContext context)
+        {
+            _dbContext = context;
+            _imageRepository = new ImageRepository(context);
+        }
+
+        public async Task AddAnswersForQuestion(int questionId, List<int> imagesIds)
+        {
+            foreach (var imageId in imagesIds)
+            {
+                ImageEntity image = await _imageRepository.GetById(imageId);
+                QuesToIm quesToIm = new QuesToIm
+                {
+                    QuestionID = questionId,
+                    ImageID= imageId,
+                  //  ThemeAnswer = answer.ThemeEntity.Id
+                };
+                await _dbContext.AddAsync(quesToIm);
+            }
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+
+
+        public async Task<int[]> GetImageFromQuestionId(int id)
+        {
+            var c = _dbContext.QuestionsToImages.Where(c => c.QuestionID == id).Select(c => c.ImageID).ToArray();
+            return c;
+        }
+    }
+}
