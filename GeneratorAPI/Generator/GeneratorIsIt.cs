520c62b0423df05d13c67a2b8c427cdc4a4dfaf3
ERIWUG <e.belyi2013@gmail.com>
2024-04-15 17:14:35 +0300

    fix:Fix isIt method

--- GeneratorAPI/Generator/GeneratorIsIt.cs
+++ GeneratorAPI/Generator/GeneratorIsIt.cs
@@ -1,61 +1,50 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Reflection;
-//using System.Text;
-//using System.Threading.Tasks;
-//using GeneratorAPI.Models;
-//using GeneratorAPI.Models.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using GeneratorAPI.Models;
+using GeneratorAPI.Models.Entities;
+using GeneratorAPI.Models.TempTable;
 
-//namespace GeneratorAPI
-//{
-//    public static partial class Generator
-//    {
-//        /// <summary>
-//        /// Function that generate and print IsItquestion
-//        /// </summary>
-//        /// <param name="mas">Array of Data</param>
-//        /// <param name="amount">Amount of Question</param>
-//        /// <Author>Belyi Egor</Author>
-//        public static Question[] GenerateIsIt(QuestionEntity[] mas, int amount)
-//        {
-//            const String APPE = "Являются ли ";
-//            String ANSW1 = "1)Являются.";
-//            String ANSW2 = "2)Не являются";
-//            String[] ANSW = { ANSW1, ANSW2 };
-//            Question[] Quest = new Question[amount];
-//            String ENDP = "";
-//            List<int> IntQuest = new List<int>();
-//            for (int i = 0; i < mas.Length - 1; i++)
-//            {
-//                if (mas[i].Type == 2)
-//                {
-//                    IntQuest.Add(i);
-//                }
-//                else
-//                {
-//                    if (mas[i].Type == 0)
-//                    {
-//                        ENDP = mas[i].Text;
-//                    }
-//                }
-//            }
-//            int l = 0;
-//            String MyHash;
-//            while (amount-- > 0)
-//            {
+namespace GeneratorAPI
+{
+    public static partial class Generator
+    {
+        /// <summary>
+        /// Function that generate and print IsItquestion
+        /// </summary>
+        /// <param name="mas">Array of Data</param>
+        /// <param name="amount">Amount of Question</param>
+        /// <Author>Belyi Egor</Author>
+        public static RezultatEntity GenerateIsIt(QuesToAns[] mas, AnswerEntity[] ans)
+        {
+            AppDbContext db = new AppDbContext();
+            ParseData(mas);
+            Random k = new Random();
+            int i = k.Next();
+            AnswerEntity n;
+            if (i % 2 == 0)
+            {
+                n = db.Answers.Where(c => c.Id == CorrectAnswerIndexes[k.Next(CorrectAnswerIndexes.Count)]).First();
+            }
+            else
+            {
+                n = db.Answers.Where(c => c.Id == IncorrectAnswerIndexes[k.Next(IncorrectAnswerIndexes.Count)]).First();
+            }
 
-//                Random m = new Random();
-//                int IA = m.Next(IntQuest.Count);
-//                int AA = IntQuest[IA];
-//                IntQuest.RemoveAt(IA);
-//                MyHash = $"DBDATA-G2-{AA}-{(mas[AA].Flag ? 0 : 1)}";
-//                Quest[l] = new Question($"{APPE} {mas[AA].Text.ToLower()} {ENDP}?", ANSW, (mas[AA].Flag ? 0 : 1), MyHash);
+            
+            var t = new RezultatEntity();
+            t.Question = mas[0].Question;
+            t.Answers.Add(n); t.Answers.Add(ans[0]); t.Answers.Add(ans[1]);
+            t.Seed = $"{t.Question.IdGroup}-{t.Question.IdSet}-{n}-ISIT-2-{ans[0].Id}-{ans[1].Id}";
 
 
-//            }
 
-//            return Quest;
-//        }
-//    }
-//}
+            return t;
+           
+        }
+    }
+}
